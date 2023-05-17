cba109ae16bd5088cab820aa0cf9a75625b02e47
mirjalol03 <jabborovmirjalol03@gmail.com>
2023-05-17 17:19:56 +0500

    Main jsx

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import Root from "./root/Root.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { rootReducer } from "./redux/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import {
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
+} from "redux";
+import ScrollToTop from "./components/ScrollToTop";
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider  store={store}>
+      <ScrollToTop />
+      <Root />
+    </Provider>
+  </BrowserRouter>
+);
