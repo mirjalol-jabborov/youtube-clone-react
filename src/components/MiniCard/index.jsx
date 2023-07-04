da1245382347f1b0b0d1c1acde8e3b22ad116dd0
Mirjalol03 <jabborovmirjalol03@gmail.com>
2023-07-04 13:17:33 +0500

    Mini card added to Project

--- src/components/MiniCard/index.jsx
+++ src/components/MiniCard/index.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import moment from "moment";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./MiniCard.scss"
 
-const MiniCard = ({data}) => {
+const MiniCard = ({ data }) => {
+  // console.log(data);
+  const navigate = useNavigate();
+  console.log(data);
   return (
-    <div className='miniCard'>
+    <div
+      className="miniCard"
+      onClick={() => {
+        navigate(`/watch/${data.id.videoId}`);
+      }}
+    >
+      <div className="miniCard__thumbnail">
+        <img src={data.snippet.thumbnails.medium.url} alt="" />
+      </div>
+      <div className="miniCard__about">
+        <div className="miniCard__about-title">
+          <p>{data.snippet.title.length > 60 ? data.snippet.title.slice(0, 60) : data.snippet.title}</p>
+        </div>
+        <div className="miniCard__about-info">
+          <span>{data.snippet.channelTitle}</span>
+          <span>
+            {moment(
+              data.snippet.publishedAt
+            ).fromNow()}
+          </span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MiniCard
+export default MiniCard;
